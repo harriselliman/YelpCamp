a5cb05d6e3f8606192583f49ae1c15f14fd22810
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-14 14:44:01 +0000

    add initial post route

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+        {name: "Salmon Creek", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104490f2c478a1e5b6bd_340.jpg"},
+        {name: "Granite Hill", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104490f2c478a1e5b6bd_340.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm4.staticflickr.com/3911/14707566622_af236f9b65.jpg"}
+    ];
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104490f2c478a1e5b6bd_340.jpg"},
-        {name: "Granite Hill", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104490f2c478a1e5b6bd_340.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm4.staticflickr.com/3911/14707566622_af236f9b65.jpg"}
-    ];
-    
     res.render("campgrounds", {campgrounds: campgrounds});
 });
 
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    
+    res.redirect("/campgrounds")
+});
 
-
-
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs")
+})
 
 
 
