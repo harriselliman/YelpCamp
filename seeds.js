67e31d407a7f313b08bcf8a42c92183c18cdc7ad
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-17 17:38:00 +0000

    add starter data array to seeds.js

--- seeds.js
+++ seeds.js
@@ -1,8 +1,26 @@
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 
+var data = [
+        {
+            name: "Cloud's Rest", 
+            image: "https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60",
+            description: "description of the campground."
+        },
+        {
+            name: "Desert Mesa", 
+            image: "https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60",
+            description: "description of the campground."
+        },
+        {
+            name: "Canyon Floor", 
+            image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60",
+            description: "description of the campground."
+        },
+    ]
 
 function seedDB(){
+    // Remove campgrounds
     Campground.remove({}, function(err){
         if(err){
             console.log(err);
@@ -10,6 +28,10 @@ function seedDB(){
         console.log("removed campgrounds");
         }
     });
+    // Add campgrounds
+    Campground.create({
+        
+    })
 }
 
 module.exports = seedDB;
