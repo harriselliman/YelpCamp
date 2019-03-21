1ada2209d5a7bae71a2b1569ae244f5845913ecd
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-21 13:07:23 +0000

    remove seedDB

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true}
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+
+
+// seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
 
