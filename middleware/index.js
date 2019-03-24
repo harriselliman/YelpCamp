84a1b107992fcebd1f1459304262df8b57bb81e2
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-24 17:48:16 +0000

    add flash error message for comment ownnership

--- middleware/index.js
+++ middleware/index.js
@@ -37,12 +37,13 @@ middlewareObj.checkCommentOwnership = function(req, res, next){
                 if(foundComment.author.id.equals(req.user._id)){
                     next();
                 } else {
+                    req.flash("error", "You don't have permission to do that.");
                     res.redirect("back");
                 }
             }
         });
     } else {
-        req.flash("error", "You need to be logged in to do that.")
+        req.flash("error", "You need to be logged in to do that.");
         res.redirect("back");
     }
 };
