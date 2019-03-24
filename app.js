3c485df0c781bcb71b2280d23c25c3ea453b0d22
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-24 15:23:08 +0000

    use connect flash

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 
 
 // seedDB(); //seed the database
