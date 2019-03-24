d403a59e39a23ebbc9909bdfaf4e38bc2d1d5be1
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-24 14:19:47 +0000

    move campground ownership middleware from campground route to middleware file

--- middleware/index.js
+++ middleware/index.js
@@ -2,5 +2,23 @@
 
 var middlewareObj = {};
 
+middlewareObj.checkCampgroundOwnership = function(req, res, next){
+    if(req.isAuthenticated()){
+        //does user own campground
+        Campground.findById(req.params.id, function(err, foundCampground){
+            if(err){
+                res.redirect("back");
+            } else {
+                if(foundCampground.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+};
 
 module.exports = middlewareObj;
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -103,23 +103,6 @@ function isLoggedIn(req, res, next){
     res.redirect("/login");
 }
 
-function checkCampgroundOwnership(req, res, next){
-    if(req.isAuthenticated()){
-        //does user own campground
-        Campground.findById(req.params.id, function(err, foundCampground){
-            if(err){
-                res.redirect("back");
-            } else {
-                if(foundCampground.author.id.equals(req.user._id)){
-                    next();
-                } else {
-                    res.redirect("back");
-                }
-            }
-        });
-    } else {
-        res.redirect("back");
-    }
-}
+
 
 module.exports = router;
