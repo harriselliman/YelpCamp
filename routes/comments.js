1b22132ba7101f9a7337895221db1d425ad532d6
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-24 14:27:32 +0000

    require middleware in comments route

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router({mergeParams: true});
-var Campground = require("../models/campground")
-var Comment = require("../models/comment")
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+var middleware = require("../middleware");
 
 
 
