695644696620cc0f941a312e850eb27dfbee31e6
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-24 14:26:52 +0000

    use middleware in campground route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,7 +18,7 @@ router.get("/", function(req, res){
 
 // CREATE - add new campground to database
 
-router.post("/", isLoggedIn, function(req, res){
+router.post("/", middleware.isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
@@ -41,7 +41,7 @@ router.post("/", isLoggedIn, function(req, res){
 
 // NEW - show form to create new campground
 
-router.get("/new", isLoggedIn,function(req, res){
+router.get("/new", middleware.isLoggedIn,function(req, res){
     res.render("campgrounds/new");
 });
 
@@ -62,7 +62,7 @@ router.get("/:id", function(req, res){
 
 //edit campground route
 
-router.get("/:id/edit", checkCampgroundOwnership,function(req, res){
+router.get("/:id/edit", middleware.checkCampgroundOwnership,function(req, res){
     Campground.findById(req.params.id, function(err, foundCampground){
         res.render("campgrounds/edit", {campground: foundCampground});
     });
@@ -70,7 +70,7 @@ router.get("/:id/edit", checkCampgroundOwnership,function(req, res){
 
 //update campground route
 
-router.put("/:id", checkCampgroundOwnership, function(req, res){
+router.put("/:id", middleware.checkCampgroundOwnership, function(req, res){
    //find and update correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
         if(err){
@@ -84,7 +84,7 @@ router.put("/:id", checkCampgroundOwnership, function(req, res){
 
 //destroy campground route
 
-router.delete("/:id", checkCampgroundOwnership, function(req, res){
+router.delete("/:id", middleware.checkCampgroundOwnership, function(req, res){
     Campground.findByIdAndRemove(req.params.id, function(err){
         if(err){
             res.redirect("/campgrounds");
