83a437c4aca922fd30d9a5a4ad03842cc0e85b24
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-25 12:04:41 +0000

    update user schema

--- models/user.js
+++ models/user.js
@@ -2,8 +2,11 @@ var mongoose = require ("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    username: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
