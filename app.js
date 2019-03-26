5a746f30b3894c5e953644abb29ce40ccfd1d002
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-26 17:24:36 +0000

    add mongolab

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes         = require("./routes/index");
     
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true}); // local DB
+mongoose.connect("mongodb+srv://harriselliman:mypassword123@cluster0-l9szf.mongodb.net/yelp_camp?retryWrites=true", { useNewUrlParser: true}) //MLab hosted DB
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
