3a4b2ed99301b3aae3c9abd225b6d47f9d1ca43d
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-26 18:10:07 +0000

    add env.DATABASE refactor

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
     
-mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true}); // local DB
-// mongoose.connect("mongodb+srv://harriselliman:mypassword123@cluster0-l9szf.mongodb.net/yelp_camp?retryWrites=true", { useNewUrlParser: true}); //MLab hosted DB
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";    
+mongoose.connect(url, { useNewUrlParser: true}); // local DB
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
